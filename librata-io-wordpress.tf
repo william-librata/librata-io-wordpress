ac7f1c1d07f2bd33dceb31bc8d35cc5aea86bc37
William Librata <william.librata@gmail.com>
2020-12-22 22:05:30 +1100

    Add terraform template for lightsail

---
+++ librata-io-wordpress.tf
@@ -0,0 +1,11 @@
+# Create a librata.io wordpress lightsail instance
+resource "aws_lightsail_instance" "librata-io-wordpress" {
+  name              = "librata-io-wordpress"
+  availability_zone = "ap-southeast-2a"
+  blueprint_id      = "wordpress"
+  bundle_id         = "micro_2_2"
+  key_pair_name     = "20180301"
+  tags = {
+    foo = "bar"
+  }
+}
